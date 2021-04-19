26f35468d93eafc0f4cb08e83043c3cd4277f2f1
amagcourse <amag.cnb1@gmail.com>
2021-04-19 18:59:08 +0700

    tambah js

---
+++ amag.js
@@ -0,0 +1,38 @@
+// dari google sheet
+const scriptURL =
+    'https://script.google.com/macros/s/AKfycbxhGHN-xqfVtb62D8ZMcuQJcHTvwqDtAWO5iOBNX8n_SzWT19gOgI6baEmb9dS7_c5Auw/exec';
+const form = document.forms['hubungikamiform']
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // ketika tombol submit di klik
+    // tampilkan tombol loading, hilangkan tombol kirim
+    btnLoading.classList.toggle('d-none');
+    btnKirim.classList.toggle('d-none');
+
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            // ketika data sukses dikirim
+            // tampilkan tombol Kirim, hilangkan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+            // Tampilkan Alert
+            myAlert.classList.toggle('d-none');
+            // Reset form
+            form.reset();
+
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+// dari google sheet akhir
+
+// tombol mati script
+
+// tombol mati script akhir
